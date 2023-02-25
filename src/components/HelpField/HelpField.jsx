66d560fe2ff5dbcdc43933a5365796edbc146136
nurgel2036 <118982282+nurgel2036@users.noreply.github.com>
2023-02-25 23:44:32 +0600

    hh

--- src/components/HelpField/HelpField.jsx
+++ src/components/HelpField/HelpField.jsx
@@ -3,15 +3,28 @@ import './HelpField.scss'
 
 import quest1 from './helpField-img/quest.png'
 
+const HelpFieldMpdal = () => {
+
+    return(
+        <div className="out-HFModal">
+            <div className="main-HFModal">
+                input
+            </div>
+        </div>
+    );
+};
+
 export default function HelpField() {
+    const [helpField,setHelpField] = React.useState(false)
   return (
     <div className='out-helpField'>
       <div className="main-helpField">
         <div className="quest-bg-image"></div>
         <div className="quest-right">
             <div className="quest-right1">Не знаете, какой курс <br /> подойдет именно вам ?</div>
-            <div className="quest-right2"></div>
-            <div className="quest-right3"></div>
+            <div className="quest-right2">Наши специалисты проконсультируют и подберут для вас нужную программу.</div>
+            <div className="quest-right3">оставить заявку</div>
+
         </div>
       </div>
     </div>
